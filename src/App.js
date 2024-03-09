6ef9d5517beb608b0be9dd85d0aca9cea11bf792
Craig Cowan <craigjcowan@gmail.com>
2024-03-08 23:31:23 -0600

    retrying change push to app engine.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>This is a test deployment on App Engine</p>
+        <p>This is a test deployment on App Engine ib 3/8</p>
         <a
           className="App-link"
           href="https://reactjs.org"
